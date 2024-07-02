f6e391eca80c71bcb1c151ac3f620d3397308ac0
WhitneySt <whitneystena418@gmail.com>
2024-07-02 12:05:09 -0500

    Agregando ejercicio sesión 20: Regex y storageAPI

--- script.js
+++ script.js
@@ -68,4 +68,69 @@ formulario.addEventListener("submit", (evento) => {
   //   const datos = obtenerDatosDeFormularios(formulario);
   const datos = obtenerDatosFormulario(formulario);
   console.log(datos);
+  const resultadoValidación = validarDatos(datos);
+  if (resultadoValidación) {
+    console.log("Datos debidamente diligenciados");
+    sessionStorage.setItem('usuario', JSON.stringify(datos));
+  }
 });
+//----------------------------EJEMPLO SESIÓN 20: Expresiones Regulares------------------
+const regex = /c.t/;
+const texto = "cuat";
+
+console.log(regex.test(texto));
+
+const regex1 = /a{5,9}/;
+
+const texto1 = "ananaaaaaaaaaaaaaaaaaaa";
+console.log(regex1.test(texto1));
+
+function validarDatos(dataForm) {
+  let validacion = true;
+
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regexPassword =
+    /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
+
+  for (const key in dataForm) {
+    const value = dataForm[key];
+    console.log(key, ": ", value);
+    if (value === "") {
+      console.log(`${key} es requerido`);
+      const span = document.createElement("span");
+      span.textContent = "* Este campo es requerido";
+      const input = document.getElementById(key);
+      input.classList.add("error");
+      insertAfter(span, input);
+      validacion = false;
+    }
+
+    if (key === "email" && !regexEmail.test(value)) {
+      console.log(`${key} no cumple con el formato de un correo electrónico`);
+      const span = document.createElement("span");
+      span.textContent = "* Debe ingresar un correo electrónico válido";
+      const input = document.getElementById(key);
+      input.classList.add("error");
+      insertAfter(span, input);
+      validacion = false;
+    }
+
+    if (key === "password" && !regexPassword.test(value)) {
+      console.log(
+        `${key} no cumple con la estructura de una contraseña segura`
+      );
+      const span = document.createElement("span");
+      const input = document.getElementById(key);
+      span.textContent =
+        "*La contraseña debe contener al menos 8 caracteres, una mayúscula, una minúscula";
+      input.classList.add("error");
+      insertAfter(span, input);
+      validacion = false;
+    }
+  }
+  return validacion;
+}
+
+function insertAfter(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
